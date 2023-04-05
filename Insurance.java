8cd1f255bad55b413239fdf165b930efec5b4cf8
Daniel isiogugu <dannydix555@gmail.com>
2023-04-05 12:54:12 +0100

    Vehicle Insurance

---
+++ Insurance.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Insurance {
+    public static void main(String[] args){
+        System.out.print("Enter your vehicle type: ");
+        System.out.println("car, truck, bus, motorcycle");
+        Scanner scan = new Scanner(System.in);
+        String type = scan.nextLine();
+        int premium = 0;
+        switch(type) {
+            case "car":
+                premium = 200;
+                break;
+            case "truck":
+                premium = 300;
+                break;
+            case "bus":
+                premium = 400;
+                break;
+            case "motorcycle":
+                System.out.println("Enter your motorcycle type: ");
+                System.out.println("1 (low power), 2 (medium), 3 (high)");
+                String motorCycleType = scan.nextLine();
+                int motorcycleInt = Integer.parseInt(motorCycleType);
+                switch(motorcycleInt) {
+                    case 1:
+                        premium = 300;
+                        break;
+                    case 2:
+                        premium = 400;
+                        break;
+                    case 3:
+                        premium = 500;
+                        break;
+                    default:
+                        System.out.println("Unknown motorcycle  type");
+                }
+                break;
+            default:
+                System.out.println("Unknown vehicle type");
+        }
+        scan.close();
+        System.out.println("The premium is $" + premium);
+    }
+}
